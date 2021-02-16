cf3ca1a867d6eb21753cc07e2888bfb6d32b160c
Uziflaona <Uziflaona@users.noreply.github.com>
2021-02-16 23:36:04 +0900

    just started to coding on Java

---
+++ LecQ/src/com/company/lQueue.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class lQueue {
+    private int index;
+    private <T> values;
+
+    public lQueue(int i) {
+        index = i;
+        values = new <T>[i];
+    }
+
+
+
+}
